36044a87631fb74f081738232aedc6714103a1ec
Filo_Puputnik <filippo.martignano@gmail.com>
2022-03-29 17:02:24 +0200

    Update plot_motif_CCCA.R

--- Scripts/Motifs/plot_motif_CCCA.R
+++ Scripts/Motifs/plot_motif_CCCA.R
@@ -11,7 +11,7 @@ get_matrix <- function(samples_info, motifs){
     file_data <- as.data.frame(readRDS(file.path(path,paste(sname,".motif.R",sep=""))))
     file_data <- subset(file_data, file_data$Var1 %in% motifs$End.motif)
     colnames(file_data)[2] <- sname
-    file_data[,2] <- as.numeric(file_data[,2]) 
+    file_data[,2] <- as.numeric(file_data[,2])
     file_data <- cbind(sname, file_data)
     file_data[,4] <- file_data[,3]/sum(file_data[,3])*100
     file_data[,5] <- samples_info[c,4]
@@ -21,17 +21,17 @@ get_matrix <- function(samples_info, motifs){
     file_data[,8] <- samples_info[c,6]
     file_data[,9] <- samples_info[c,8]
     colnames(file_data) <- c("samplename", "motif","count", "normalizedToCounts", "disease", "type","basename", "library","tumorFrac")
-    
+
     if (c == 1){
       total <- file_data
     } else {
       total <- rbind(total, file_data)
-    } 
+    }
   }
   return(total)
 }
 #### load sample annotation file
-samples_info <- read.table("~/Fragmentomics/samples_info_heatmap.tsv", sep="\t", header=F, stringsAsFactors = F)
+samples_info <- read.table("~/Fragmentomics/Utility/samples_info_heatmap.tsv", sep="\t", header=F, stringsAsFactors = F)
 
 #### select motif order
 motifNames <- read.delim("~/Fragmentomics/Utility/endmotifs.txt", header = T, sep = "\t")
@@ -58,8 +58,8 @@ order_list <- order_list[which(order_list %in% data$disease)]
 hashcol <- c("HU" ="#74add1" ,
              "Healthy"    ="#2c7bb6" ,
              "ISPRO"    ="#2c7bb6" ,
-             "TF<0.15"    =rgb(211/(255),119/(255),102/(255),1), 
-             "TF>0.15"    =rgb(105/(255),58/(255),29/(255),1), 
+             "TF<0.15"    =rgb(211/(255),119/(255),102/(255),1),
+             "TF>0.15"    =rgb(105/(255),58/(255),29/(255),1),
              "19-326"     = "black"     ,
              "Illumina"=  "#fdc086"         )
 
@@ -74,21 +74,21 @@ p<-ggplot(data, aes_string(x=x, y=y, color=x)) + xlab(" ") + ylab("Motif %") +
   stat_summary(fun.data=mean_se, geom="errorbar", color="black",width=0.25) +
   scale_color_brewer("",palette="Dark2") +
   theme_classic() +
-  ggtitle("CCCA Motif") + 
+  ggtitle("CCCA Motif") +
   theme(plot.title = element_text(size = 20, face = "bold"))+
   theme(legend.position="none",
         axis.text.x = element_text(size=font*0.9),
         axis.text.y = element_text(size=font),
         axis.title = element_text(size=font),
-        panel.grid.minor = element_blank(), 
+        panel.grid.minor = element_blank(),
         panel.grid.major = element_blank(),
-  ) + scale_x_discrete(limits=order_list) + ylim(0,2.3) + scale_color_manual(values = colors <- hashcol) 
+  ) + scale_x_discrete(limits=order_list) + ylim(0,2.3) + scale_color_manual(values = colors <- hashcol)
 
-#### add P-value 
+#### add P-value
 if ("Healthy" %in% data$disease){
-  p<- p + stat_compare_means(method = "t.test", size = 8, comparisons = list(c("Healthy", "TF>0.15"))) 
+  p<- p + stat_compare_means(method = "t.test", size = 8, comparisons = list(c("Healthy", "TF>0.15")))
 } else {
-  p<- p + stat_compare_means(method = "t.test", size = 8, comparisons = list(c("ISPRO", "TF>0.15"))) 
+  p<- p + stat_compare_means(method = "t.test", size = 8, comparisons = list(c("ISPRO", "TF>0.15")))
 }
 
 #### save plot
